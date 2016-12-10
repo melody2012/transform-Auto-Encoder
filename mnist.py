d6dc3462e09d9d8a97f770040c9d30db79090f4b
maozi <xmiller@vip.qq.com>
2016-12-10 20:38:39 +0800

    finished first version

--- mnist.py
+++ mnist.py
@@ -1,18 +1,21 @@
-from keras.datasets import mnist
+# -*-coding: utf-8-*-
+
+import gzip
+import cPickle
 import random
 import numpy as np
 
+
 class dataset:
     def __init__(self):
-        print('Loading data')
-        (self.image_train, _), (self.image_test, _) = mnist.load_data()
+        with gzip.open('./MNIST-data/mnist.pkl.gz', 'rb') as f:
+            (self.image_train, _), (self.image_test, _) = cPickle.load(f)
         self.image_train = self.image_train.reshape(60000, 784)
         self.image_test = self.image_test.reshape(10000, 784)
         self.image_train = self.image_train.astype('float32')
         self.image_test = self.image_test.astype('float32')
         self.image_train /= 255
         self.image_test /= 255
-        print('Done')
 
     def next_batch(self, batchSize, delta):
         input_images = self.image_train[random.sample(range(0, 60000), batchSize), :]
@@ -21,13 +24,10 @@ class dataset:
         for i in range(batchSize):
             for y in range(28):
                 for x in range(28):
-                    new_x = x + delta[0];
-                    new_y = y + delta[1]
-                    if new_x < 28 and new_y < 28:
+                    new_x = x + delta[0][0];
+                    new_y = y + delta[0][1]
+                    if new_x < 28 and new_y < 28 and new_x >= 0 and new_y >= 0:
                         output_image[i][new_x + new_y * 28] = input_images[i][x + y * 28]
 
-        return input_images, output_image
-
-    def text_image(self):
-        pass
+        return input_images, input_images
 
--- tae.py
+++ tae.py
@@ -1,76 +1,93 @@
+# -*-coding: utf-8-*-
+
 import tensorflow as tf
 import mnist
 import numpy as np
 from tqdm import tqdm
 
-IMAGE_PIXELS = 28 * 28
+IMAGE_PIXELS = 784
 NB_CAPSULE = 30
 NB_HIDDEN1_UNITS = 10
 NB_HIDDEN2_UNITS = 20
-NB_INSTANTIA_PARAMS = 3
+NB_INSTANTIA_PARAMS = 3     # 特征数量+1(包括p)
 
 BATCH_SIZE = 32
-NB_TRAIN_EPOCH = 100
-LEARNING_RATE = 0.01
+TRAIN_EPOCH = 10000
+LEARNING_RATE = 1e-4
+SAVE_PERIOD = 2000
 
 def dense_layer(_x, nb_last_units, nb_this_units):
-    weights = tf.Variable(
-        tf.truncated_normal([nb_last_units, nb_this_units],
+    # 产生一个全连接层
+    weights = tf.Variable(tf.truncated_normal([nb_last_units, nb_this_units],
                             stddev=1.0 / np.sqrt(float(nb_last_units))))
-    biases = tf.Variable(tf.zeros([nb_this_units]))
+    biases = tf.Variable(tf.zeros([nb_this_units]))     # 偏置
     layer = tf.nn.relu(tf.matmul(_x, weights) + biases)
 
     return layer
 
 
 def capsule(images, delta):
-    hidden1 = dense_layer(images, IMAGE_PIXELS, NB_HIDDEN1_UNITS)
+    # 产生一个胶囊
+    hidden1 = dense_layer(images, IMAGE_PIXELS, NB_HIDDEN1_UNITS)   # 首先是第一个隐层，连接图片和胶囊
 
+    # 然后是第二个隐层，把图片转化成某种高层次表达（encoder）
     hidden2 = dense_layer(hidden1, NB_HIDDEN1_UNITS, NB_INSTANTIA_PARAMS)
 
     # x, y, p
-    p = hidden2[2]
-    instatiation_params = hidden2[0:2]
-    instatiation_params[0] += delta[0]
-    instatiation_params[1] += delta[1]
+    p = [hidden2[:, 2]]     # 这个胶囊的权重
+    instantiation_params = hidden2[:, 0:2]  # 高层次表达实例化参数（这里是x，y坐标）
+    instantiation_params += delta       # 对其应用某种改变（这里是平移）
 
-    hidden3 = dense_layer(instatiation_params, NB_INSTANTIA_PARAMS - 1, NB_HIDDEN2_UNITS)
+    # 重新生成图片（decoder）
+    hidden3 = dense_layer(instantiation_params, NB_INSTANTIA_PARAMS - 1, NB_HIDDEN2_UNITS)
 
-    capsule_output_images = dense_layer(hidden3, NB_HIDDEN2_UNITS, IMAGE_PIXELS) * p
+    # 利用p重新计算输出图片
+    capsule_output_images = tf.matmul(p, dense_layer(hidden3, NB_HIDDEN2_UNITS, IMAGE_PIXELS))
 
     return capsule_output_images
 
-def loss(logits, labels):
-  cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels)
-  loss = tf.reduce_mean(cross_entropy)
-  return loss
+def loss(predicts, labels):
+    # mse loss
+    loss = tf.reduce_mean(tf.square(predicts - labels))
+    return loss
 
 def __main__():
 
+    print "building model"
+    # 输入输出占位符
     input_images_placeholder = tf.placeholder(tf.float32, shape=(BATCH_SIZE, IMAGE_PIXELS))
     input_deltas_placeholder = tf.placeholder(tf.float32, shape=(BATCH_SIZE, NB_INSTANTIA_PARAMS - 1))
     output_images_placeholder = tf.placeholder(tf.float32, shape=(BATCH_SIZE, IMAGE_PIXELS))
 
-    output = tf.Variable(tf.zeros([IMAGE_PIXELS, IMAGE_PIXELS]))
+    # 用来接收累加各个胶囊输出
+    output = tf.Variable(tf.zeros([BATCH_SIZE, IMAGE_PIXELS]))
     for i in range(NB_CAPSULE):
         output += capsule(input_images_placeholder, input_deltas_placeholder)
 
+    # loss操作符
     loss_op = loss(output, output_images_placeholder)
 
+    # 批随机梯度下降优化器
     optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE)
     train = optimizer.minimize(loss_op)
     init = tf.global_variables_initializer()
 
     session = tf.Session()
-
-    session.run(init)
+    session.run(init)   # 初始化
 
     saver = tf.train.Saver()
 
-    train_set = mnist.dataset()
+    print('Loading data')
+    train_set = mnist.dataset()     # mnist数据集
 
-    for i in tqdm(range(NB_TRAIN_EPOCH)):
-        delta = np.zeros([2])
+    # 进度条
+    progress_bar = tqdm(xrange(TRAIN_EPOCH))
+
+    print "start training"
+
+    for i in progress_bar:
+        #delta = np.zeros([BATCH_SIZE, 2])
+        delta = np.random.randint(low=-3, high=4, size=(BATCH_SIZE, NB_INSTANTIA_PARAMS - 1))   # 随机位移
         images_feed, labels_feed = train_set.next_batch(BATCH_SIZE, delta)
         feed_dict = {
             input_images_placeholder: images_feed,
@@ -78,9 +95,10 @@ def __main__():
             output_images_placeholder: labels_feed,
         }
         _, loss_value = session.run([train, loss_op], feed_dict=feed_dict)
-        if i % 10 == 0:
-            print "loss", loss_value
-            saver.save(session, "./model")
+        progress_bar.set_description("loss = %.5f" % loss_value)
+        if i % SAVE_PERIOD == 0:
+            saver.save(session, "./model/model")
+    print "done"
 
 
 if __name__ == "__main__":
